9f45642fe538be4c4df860caee2161c93e418b8b
ggavarra <gayathri1.g@gmail.com>
2018-12-26 11:48:55 +0800

    First Commit

---
+++ src/main/java/com/example/ggpetclinic/GgPetClinicApplication.java
@@ -0,0 +1,14 @@
+package com.example.ggpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GgPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GgPetClinicApplication.class, args);
+	}
+
+}
+
